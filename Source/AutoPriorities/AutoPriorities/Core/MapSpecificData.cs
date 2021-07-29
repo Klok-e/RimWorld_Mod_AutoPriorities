9f3e2d6af71e041a973520240b4ee82de4a7e48d
LokiVKlokeNaAndoke <ma.ba1@rambler.ru>
2021-07-29 19:58:15 +0300

    comment out map specific

--- Source/AutoPriorities/AutoPriorities/Core/MapSpecificData.cs
+++ Source/AutoPriorities/AutoPriorities/Core/MapSpecificData.cs
@@ -2,16 +2,16 @@ using Verse;
 
 namespace AutoPriorities.Core
 {
-    public class MapSpecificData : MapComponent
-    {
-        public MapSpecificData(Map map)
-            : base(map)
-        {
-        }
-
-        public override void ExposeData()
-        {
-            base.ExposeData();
-        }
-    }
+    // public class MapSpecificData : MapComponent
+    // {
+    //     public MapSpecificData(Map map)
+    //         : base(map)
+    //     {
+    //     }
+    //
+    //     public override void ExposeData()
+    //     {
+    //         base.ExposeData();
+    //     }
+    // }
 }
